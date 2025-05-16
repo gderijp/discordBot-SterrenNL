1174867228c1a2f08a9ca1fe3d9087e393e88621
GIORGIO <gderijp@gmail.com>
2025-05-16 22:42:07 +0200

    switched from php to node.js bot, add pingpong, add command deployer

--- key.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Key
-{
-    private $key = 'MTM3MDQyNDIxMTcyNTYxNTEzNA.Gn25sC.BGQYUUIp6H1lZgTntRQN29KqcYn2gY1V4cNRbw';
-    public function getKey()
-    {
-        return $this->key;
-    }
-}
